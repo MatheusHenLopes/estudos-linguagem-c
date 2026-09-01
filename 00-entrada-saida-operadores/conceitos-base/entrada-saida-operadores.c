ffd1eae5dd0f4c068f53c2cfa8240f6a5894a98c
MatheusHenLopes <matheushenriquelopes24@gmail.com>
2026-09-01 16:19:23 -0300

    fix: corrige linha e escrita do comentario.

--- 00-entrada-saida-operadores/conceitos-base/entrada-saida-operadores.c
+++ 00-entrada-saida-operadores/conceitos-base/entrada-saida-operadores.c
@@ -16,7 +16,7 @@ int main() {
     printf("%lf\n", number3);
     printf("%s\n", nome);
 
-    // exemplo de construção
+    // exemplo de construção usando todos
 
     printf("O estudante %s, sabe que o valor de PI e aproximadamente %f e calculou o log de %d, o resultado foi de %lf", nome, number2, number1, number3);
 
